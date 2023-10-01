91fa6a0eb4a718cf9d360d7f891f99be7fa22449
Lucas <caslumach@gmail.com>
2023-09-30 23:28:50 -0300

    ajustes

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ document.addEventListener("DOMContentLoaded", function () {
     const Insights = document.getElementById("Insights");
     Insights.addEventListener("click", function () {
         window.location.href = "pages/insights/insights.html"
-        alert('clicou')
     });
 });
