a0688d1b478074a86bdd52f5918ddc3d2b3dcc80
Lucas <caslumach@gmail.com>
2023-10-01 20:39:15 -0300

    ajustes textis

--- pages/insights/insights.js
+++ pages/insights/insights.js
@@ -18,12 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
             title: "Venda de Crédito de carbono.",
             text: `Um crédito de carbono vale aproximadamente US$ 5 ou R$ 26 no Brasil. O preço mundial do crédito de carbono, no entanto, pode variar de US$ 1,20 a US$ 40 por tonelada de carbono, dependendo do mercado que está sendo negociado.`,
             text1: `Um crédito de carbono é gerado a cada tonelada de carbono que deixa de ser emitida ou é capturada da atmosfera. A partir de regras e metodologias previamente selecionadas e validadas, é analisado se os carros estão realmente reduzindo as emissões.`,
-            text2: `Um cliente procura uma empresa responsável pela venda de créditos de carbono. A partir da contratação da empresa, é formado um inventário de gases do efeito estufa produzidos pelo cliente`,
+            text2: `Um cliente procura uma empresa responsável pela venda de créditos de carbono.
+             A partir da contratação da empresa, é formado um inventário de gases do efeito estufa produzidos pelo cliente.`,
             link: '../../assets/high-angle-small-tree-stack-coins.jpg'
         },
         {
             title: "Venda e Analise de Dados.",
-            text: `Um veículo elétrico gera dados a partir do momento em que ele é ligado. A análise de dados pode agregar valor e os dados se tornarem atrativos para serem vendidos para fabricantes de veículos elétricos, empresas de energia ou outras partes interessadas que buscam informações e métricas sobre o mercado de veículos elétricos`,
+            text: `Um veículo elétrico gera dados a partir do momento em que ele é ligado. A análise de dados pode agregar valor e os dados se tornarem atrativos para serem vendidos para fabricantes de veículos elétricos, empresas de energia ou outras partes interessadas que buscam informações e métricas sobre o mercado de veículos elétricos.`,
             text1: `Com a integração do veículo elétrico ao sistema MEV-Fleet, seria possível realizar a tratativa dos dados em tempo real.`,
             text2:`Além de outras possibilidades também para empresas de marketing que estudam o comportamento do consumidor.`,
             link: '../../assets/pexels-anna-nekrashevich-6802049.jpg'
@@ -32,7 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
             title: "Venda de seguro de veículos elétricos.",
             text: `Os veículos elétricos exigem seguros específicos devido às suas características e tecnologias. A MEV-Fleet pode oferecer seguros especializados para veículos elétricos, fornecendo cobertura adequada e gerando receita adicional.`,
             text1: `Por se tratar de um seguro de frota de veículos elétricos, com volume de clientes da base e seguradoras parceiras, a MEV-Fleet poderá oferecer melhores preços de seguros.`,
-            text2: `Com o avanço da tecnologia cada vez mais vemos o número de carros elétricos rodando no Brasil. Um novo relatório do Rocky Mountain Institute (RMI) prevê que os veículos elétricos estão se popularizando tanto que dominarão o mercado global até o final desta década. De acordo com o documento, os modelos serão de 62% a 86% das vendas até 2030.`,
+            text2: `Com o avanço da tecnologia cada vez mais vemos o número de carros elétricos rodando no Brasil.
+             Um novo relatório do Rocky Mountain Institute (RMI) prevê que os veículos elétricos estão se popularizando tanto que dominarão o mercado global até o final desta década.
+              De acordo com o documento, os modelos serão de 62% a 86% das vendas até 2030.`,
             link: "../../assets/close-up-no-carregamento-do-carro-eletrico.jpg"
         },
     ];
